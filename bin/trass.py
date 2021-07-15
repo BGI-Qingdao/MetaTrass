8413399795f7ff0f363e6386beb6478ac1bb6612
Yanwei Qi <qiyanweii@icloud.com>
2021-07-15 14:24:02 +0800

    trass parser adding

--- bin/trass.py
+++ bin/trass.py
@@ -23,6 +23,16 @@ import MetaTrass.GC as GC
 import MetaTrass.TB as TB
 import MetaTrass.AP as AP
 
+import MetaTrass.ToolConfig as ToolConfig
+
+import MetaTrass.SplitBarcode as SplitBarcode
+import MetaTrass.GetCleandata as GetCleandata
+import MetaTrass.Kraken2Taxon as Kraken2Taxon
+import MetaTrass.TAB_refining as TAB_refining
+import MetaTrass.ReadID2Fastq as ReadID2Fastq
+import MetaTrass.MetaAssembly as MetaAssembly
+import MetaTrass.ContigPurify as ContigPurify
+
 def version():
     version_file = open('%s/VERSION' % MetaTrass_config.config_file_path)
     return version_file.readline().strip()
@@ -78,61 +88,79 @@ if __name__ == '__main__':
 
     ######################################### define arguments for subparsers ##########################################
 
-    # add arguments for PI_parser
+    # add arguments for GC_parser
     GC_parser.add_argument('-i',       required=True,                       help='input genome folder')
     GC_parser.add_argument('-o',       required=False,                      help='taxonomic classification of input genomes')
     GC_parser.add_argument('-p',       required=True,                       help='output prefix')
     GC_parser.add_argument('-r',       required=False, default=None,        help='grouping rank, choose from p (phylum), c (class), o (order), f (family), g (genus) or any combination of them')
-    PI_parser.add_argument('-g',       required=False, default=None,        help='grouping file')
-    PI_parser.add_argument('-x',       required=False, default='fasta',     help='file extension')
-    PI_parser.add_argument('-nonmeta', required=False, action="store_true", help='provide if input genomes are NOT metagenome-assembled genomes')
-    PI_parser.add_argument('-t',       required=False, type=int, default=1, help='number of threads, default: 1')
-    PI_parser.add_argument('-quiet',   required=False, action="store_true", help='not report progress')
-    PI_parser.add_argument('-force',   required=False, action="store_true", help='force overwrite existing results')
-    PI_parser.add_argument('-noblast', required=False, action="store_true", help='skip running all-vs-all blastn, provide if you have other ways (e.g. with job scripts) to speed up the blastn step')
 
-    # add arguments for BP_parser
-    BP_parser.add_argument('-p',             required=True,                                help='output prefix')
-    BP_parser.add_argument('-r',             required=False, default=None,                 help='grouping rank')
-    BP_parser.add_argument('-g',             required=False, default=None,                 help='grouping file')
-    BP_parser.add_argument('-cov',           required=False, type=int,     default=75,     help='coverage cutoff, default: 75')
-    BP_parser.add_argument('-al',            required=False, type=int,     default=200,    help='alignment length cutoff, default: 200')
-    BP_parser.add_argument('-flk',           required=False, type=int,     default=10,     help='the length of flanking sequences to plot (Kbp), default: 10')
-    BP_parser.add_argument('-ip',            required=False, type=int,     default=90,     help='identity percentile cutoff, default: 90')
-    BP_parser.add_argument('-ei',            required=False, type=float,   default=80,     help='end match identity cutoff, default: 80')
-    BP_parser.add_argument('-t',             required=False, type=int,     default=1,      help='number of threads, default: 1')
-    BP_parser.add_argument('-NoEbCheck',     required=False, action="store_true",          help='disable end break and contig match check for fast processing, not recommend for metagenome-assembled genomes (MAGs)')
-    BP_parser.add_argument('-force',         required=False, action="store_true",          help='overwrite previous results')
-    BP_parser.add_argument('-quiet',         required=False, action="store_true",          help='Do not report progress')
-    BP_parser.add_argument('-tmp',           required=False, action="store_true",          help='keep temporary files')
+    # add arguments for TB_parser
+    TB_parser.add_argument('-p',             required=True,                                help='output prefix')
+    TB_parser.add_argument('-r',             required=False, default=None,                 help='grouping rank')
+    TB_parser.add_argument('-g',             required=False, default=None,                 help='grouping file')
+    TB_parser.add_argument('-cov',           required=False, type=int,     default=75,     help='coverage cutoff, default: 75')
+    TB_parser.add_argument('-al',            required=False, type=int,     default=200,    help='alignment length cutoff, default: 200')
+    TB_parser.add_argument('-flk',           required=False, type=int,     default=10,     help='the length of flanking sequences to plot (Kbp), default: 10')
+    TB_parser.add_argument('-ip',            required=False, type=int,     default=90,     help='identity percentile cutoff, default: 90')
 
-    # add arguments for filter_HGT_parser
-    filter_HGT_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
-    filter_HGT_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
-    filter_HGT_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
-    filter_HGT_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
-    filter_HGT_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+    # add arguments for AP_parser
+    AP_parser.add_argument('-p',             required=True,                                help='output prefix')
+    AP_parser.add_argument('-r',             required=False, default=None,                 help='grouping rank')
+    AP_parser.add_argument('-g',             required=False, default=None,                 help='grouping file')
+    AP_parser.add_argument('-cov',           required=False, type=int,     default=75,     help='coverage cutoff, default: 75')
+    AP_parser.add_argument('-al',            required=False, type=int,     default=200,    help='alignment length cutoff, default: 200')
+    AP_parser.add_argument('-flk',           required=False, type=int,     default=10,     help='the length of flanking sequences to plot (Kbp), default: 10')
+    AP_parser.add_argument('-ip',            required=False, type=int,     default=90,     help='identity percentile cutoff, default: 90')
 
-    # add arguments for update_hmm
-    update_hmms_parser.add_argument('-hmm',             required=True,                          help='MetaCHIP_phylo.hmm file')
-    update_hmms_parser.add_argument('-p_db',            required=False, default=None,           help='Pfam db file, e.g. Pfam-A.hmm')
-    update_hmms_parser.add_argument('-t_db',            required=False, default=None,           help='TIGRFAMs db folder, e.g. TIGRFAMs_14.0_HMM')
+    # add arguments for SplitBarcode_parse
+    SplitBarcode_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    SplitBarcode_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    SplitBarcode_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    SplitBarcode_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    SplitBarcode_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
 
-    # add arguments for get_SCG_tree
-    get_SCG_tree_parser.add_argument('-i',              required=True,                          help='input genome folder')
-    get_SCG_tree_parser.add_argument('-p',              required=True,                          help='output prefix')
-    get_SCG_tree_parser.add_argument('-x',              required=False, default='fasta',        help='file extension')
-    get_SCG_tree_parser.add_argument('-nonmeta',        required=False, action="store_true",    help='annotate Non-metagenome-assembled genomes (Non-MAGs)')
-    get_SCG_tree_parser.add_argument('-t',              required=False, type=int, default=1,    help='number of threads, default: 1')
+    # add argument for GetCleandata_parser
+    GetCleandata_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    GetCleandata_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    GetCleandata_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    GetCleandata_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    GetCleandata_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
 
-    # add arguments for rename_seqs
-    rename_seqs_parser.add_argument('-in',         required=True,                          help='input sequence file')
-    rename_seqs_parser.add_argument('-inc_suffix', required=False, action="store_true",    help='rename sequences by incrementally adding suffix to file name')
-    rename_seqs_parser.add_argument('-sep_in',     required=False, default=None,           help='separator for input sequences')
-    rename_seqs_parser.add_argument('-sep_out',    required=False, default=None,           help='separator for output sequences, default: same as sep_in')
-    rename_seqs_parser.add_argument('-n',          required=False, default=None, type=int, help='the number of columns to keep')
-    rename_seqs_parser.add_argument('-prefix',     required=False, default=None,           help='add prefix to sequence')
-    rename_seqs_parser.add_argument('-x',          required=False,                         help='file extension')
+    # add argument for Kraken2Taxon_parser 
+    Kraken2Taxon_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    Kraken2Taxon_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    Kraken2Taxon_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    Kraken2Taxon_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    Kraken2Taxon_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+
+    # add argument for TAB_refining_parser
+    TAB_refining_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    TAB_refining_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    TAB_refining_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    TAB_refining_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    TAB_refining_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+
+    # add argument for ReadID2Fastq_parser
+    ReadID2Fastq_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    ReadID2Fastq_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    ReadID2Fastq_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    ReadID2Fastq_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    ReadID2Fastq_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+
+    # add argument for MetaAssembly_parser
+    MetaAssembly_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    MetaAssembly_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    MetaAssembly_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    MetaAssembly_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    MetaAssembly_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+
+    # add argument for ContigPurify_parser
+    ContigPurify_parser.add_argument('-i',                required=True,                          help='txt file containing detected HGTs, e.g. [prefix]_[ranks]_detected_HGTs.txt ')
+    ContigPurify_parser.add_argument('-n',                required=True, type=int,                help='HGTs detected at least n levels, 2 <= n <= 5')
+    ContigPurify_parser.add_argument('-plot',             required=False,                         help='flanking plots folder')
+    ContigPurify_parser.add_argument('-ffn',              required=False, default=None,           help='get nucleotide sequences for qualified HGTs')
+    ContigPurify_parser.add_argument('-faa',              required=False, default=None,           help='get amino acid sequences for qualified HGTs')
+
 
 
     ############################## parse provided arguments and run corresponding function #############################
@@ -146,11 +174,11 @@ if __name__ == '__main__':
     else:
         args = vars(parser.parse_args())
 
-    if args['subparser_name'] == 'PI':
-        PI(args, MetaCHIP_config.config_dict)
+    if args['subparser_name'] == 'GC':
+        GC(args, MetaTrass_config.config_dict)
 
-    if args['subparser_name'] == 'BP':
-        BP(args, MetaCHIP_config.config_dict)
+    if args['subparser_name'] == 'TB':
+        TB(args, MetaCHIP_config.config_dict)
 
     if args['subparser_name'] == 'filter_HGT':
         filter_HGT.filter_HGT(args)
